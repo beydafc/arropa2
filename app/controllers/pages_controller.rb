6b2af1a898915c37bbbafc925c49f05ba44123ac
Luis Camilo <luiscamilov@hotmail.com>
2023-12-21 09:40:55 -0500

    avances en items creation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: :profile
   def home
     @items = Item.all
+  end
+
+  def profile
+  end
+  def user_items
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "profile", to: "pages#profile"
+  get "user_items", to: "pages#user_items"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
