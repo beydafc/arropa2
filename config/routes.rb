7ee1c60e9abd499692effa0f43fdbeeba38fdda6
Luis Camilo <luiscamilov@hotmail.com>
2023-12-22 10:32:34 -0500

    form

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,8 @@ Rails.application.routes.draw do
   get "profile", to: "pages#profile"
   get "user_items", to: "pages#user_items"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "pages#home"
   resources :items do
     collection do
       get :top
